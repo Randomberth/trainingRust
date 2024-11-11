064e15edb5c8214a44c57199631bed0120d4fb93
Alibert <aliberth33@gmail.com>
2024-11-11 14:18:41 -0400

    controlFlow rust training (1)

---
+++ functions/target/debug/deps/functions-c6796017ae192bd2.d
@@ -0,0 +1,5 @@
+/home/randomberth/RustTest/trainingRust/functions/target/debug/deps/libfunctions-c6796017ae192bd2.rmeta: src/main.rs
+
+/home/randomberth/RustTest/trainingRust/functions/target/debug/deps/functions-c6796017ae192bd2.d: src/main.rs
+
+src/main.rs:
