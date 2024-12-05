1db07c6d12689015fcf51e2d0cec7a52001029cc
Alibert <aliberth33@gmail.com>
2024-12-05 18:18:07 -0400

    finish training of implement

--- implement/src/main.rs
+++ implement/src/main.rs
@@ -1,19 +1,40 @@
 struct Person {
     name: String,
+    profession: String,
 }
 
-impl Person {
+trait Human {
+    fn is_human(&self) -> bool;
+    fn print_name(&self);
+    fn print_profession(&self);
+    fn new(name: String, profession: String) -> Self;
+}
+
+impl Human for Person {
+    fn is_human(&self) -> bool {
+        true
+    }
+
     fn print_name(&self) {
         println!("  Name:  {}", self.name)
+    }
+
+    fn print_profession(&self) {
+        println!("  Profession:  {}", self.profession)
+    }
+
+    fn new(name: String, profession: String) -> Self {
+        Self { name, profession }
     }
 }
 
 fn main() {
-    let person = Person {
-        name: String::from("Papito mi rey fiu fiu"),
-    };
+    let person = Person::new(String::from("Randomberth"), String::from("AliAli"));
 
     println!("");
     person.print_name();
     println!("");
+    person.print_profession();
+    println!("");
+    println!("Is person human? {}", person.is_human());
 }
