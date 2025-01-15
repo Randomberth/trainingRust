97bbf2ce156ca5eabf40dfe0c331efac940a6361
Aliberth <aliberth@kaliRandomberth.org>
2025-01-15 09:14:43 -0400

    init pinhon program

---
+++ pinhon/target/debug/deps/pinhon-c44eab669942459c.d
@@ -0,0 +1,5 @@
+/home/aliberth/rust_test/pinhon/target/debug/deps/libpinhon-c44eab669942459c.rmeta: src/main.rs
+
+/home/aliberth/rust_test/pinhon/target/debug/deps/pinhon-c44eab669942459c.d: src/main.rs
+
+src/main.rs:
---
+++ pinhon/target/debug/deps/pinhon-e89fe46512c31a4e.d
@@ -0,0 +1,5 @@
+/home/aliberth/rust_test/pinhon/target/debug/deps/libpinhon-e89fe46512c31a4e.rmeta: src/main.rs
+
+/home/aliberth/rust_test/pinhon/target/debug/deps/pinhon-e89fe46512c31a4e.d: src/main.rs
+
+src/main.rs:
